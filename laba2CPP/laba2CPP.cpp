7ab619320e88c2b472fe73df5c55f51424c568c8
Алексей Гончар <alekseyhonchar@gmail.com>
2020-10-24 10:47:28 +0300

    Add project files.

---
+++ laba2CPP/laba2CPP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a1, b1, c1, a2, b2, c2;
+    bool haveNoSol, haveOneSol, haveManySol;
+    cout << "Input a1\n"; cin >> a1; //коэф. а первого уравнения
+    cout << "Input a2\n"; cin >> a2; //коэф. a второго уравнения
+    cout << "Input b1\n"; cin >> b1; //коэф. b первого уравнения
+    cout << "Input b2\n"; cin >> b2; //коэф. b второго уравнения
+    cout << "Input c1\n"; cin >> c1; //коэф. c первого уравнения
+    cout << "Input c2\n"; cin >> c2; //коэф. c второго уравнения
+    if ((a1 / a2) == (b1 / b2) && (a1 / a2)  != (c1 / c2)) {
+        cout << "System has no any solutions.\n";
+        haveNoSol = true;
+    }
+    else {
+        if ((a1 / a2) == (b1 / b2) && (a1 / a2) == (c1 / c2)) {
+            cout << "System has infinity solutions.\n";
+            haveManySol = true;
+        }
+        else {
+            cout << "System has only 1 solution.\n";
+            haveOneSol = true;
+        }
+    }
+    system("pause");
+}
