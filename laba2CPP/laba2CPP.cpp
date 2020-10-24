2193ccace323ee4ecd5f48aaf0367127adadf78f
Oleksii Honchar <63127318+hxnchar@users.noreply.github.com>
2020-10-24 10:51:36 +0300

    Update laba2CPP.cpp

--- laba2CPP/laba2CPP.cpp
+++ laba2CPP/laba2CPP.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     double a1, b1, c1, a2, b2, c2;
     bool haveNoSol, haveOneSol, haveManySol;
-    cout << "Input a1\n"; cin >> a1; //коэф. а первого уравнения
-    cout << "Input a2\n"; cin >> a2; //коэф. a второго уравнения
-    cout << "Input b1\n"; cin >> b1; //коэф. b первого уравнения
-    cout << "Input b2\n"; cin >> b2; //коэф. b второго уравнения
-    cout << "Input c1\n"; cin >> c1; //коэф. c первого уравнения
-    cout << "Input c2\n"; cin >> c2; //коэф. c второго уравнения
+    cout << "Input a1\n"; cin >> a1; //ГЄГ®ГЅГґ. Г  ГЇГҐГ°ГўГ®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
+    cout << "Input a2\n"; cin >> a2; //ГЄГ®ГЅГґ. a ГўГІГ®Г°Г®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
+    cout << "Input b1\n"; cin >> b1; //ГЄГ®ГЅГґ. b ГЇГҐГ°ГўГ®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
+    cout << "Input b2\n"; cin >> b2; //ГЄГ®ГЅГґ. b ГўГІГ®Г°Г®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
+    cout << "Input c1\n"; cin >> c1; //ГЄГ®ГЅГґ. c ГЇГҐГ°ГўГ®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
+    cout << "Input c2\n"; cin >> c2; //ГЄГ®ГЅГґ. c ГўГІГ®Г°Г®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї
     if ((a1 / a2) == (b1 / b2) && (a1 / a2)  != (c1 / c2)) {
         cout << "System has no any solutions.\n";
         haveNoSol = true;
